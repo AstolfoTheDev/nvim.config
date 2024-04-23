ce23584b73ad1ed7ced5a126cabe8aa60fee91a0
AstolfoTheDev <ropankatz@gmail.com>
2024-04-23 23:22:39 +0200

    Lazygit leader changed and lazy loading enabled

--- lua/kickstart/plugins/lazygit.lua
+++ lua/kickstart/plugins/lazygit.lua
@@ -1,22 +1,23 @@
 return {
-    {
-      'kdheepak/lazygit.nvim',
-      version = '*',
-      cmd = {
-        'LazyGit',
-        'LazyGitConfig',
-        'LazyGitCurrentFile',
-        'LazyGitFilter',
-        'LazyGitFilterCurrentFile',
-      },
-      -- optional for floating window border decoration
-      dependencies = {
-        'nvim-lua/plenary.nvim',
-      },
-      -- setting the keybinding for LazyGit with 'keys' is recommended in
-      -- order to load the plugin when the command is run for the first time
-      keys = {
-        { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-      },
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    version = '*',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
     },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
 }
