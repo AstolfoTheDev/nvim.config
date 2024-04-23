a5e94935500bc246f01ef5a9c497bdeea503e5ee
AstolfoTheDev <ropankatz@gmail.com>
2024-04-23 23:44:20 +0200

    Vimtex added need o configure

--- init.lua
+++ init.lua
@@ -795,6 +795,7 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.lazygit',
+  require 'kickstart.plugins.vimtex',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
